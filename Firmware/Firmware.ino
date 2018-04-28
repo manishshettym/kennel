d2b3c112cebb536c6db2f2bc4f22d1e2db3b190b
MA <mmshetty.98@gmail.com>
2018-04-28 07:27:38 +0530

    Add files via upload

---
+++ Firmware/Firmware.ino
@@ -0,0 +1,225 @@
+// Include Libraries
+#include "Arduino.h"
+#include "NewPing.h"
+#include "LDR.h"
+#include "RFID.h"
+#include "Wire.h"
+#include "RTClib.h"
+#include "Servo.h"
+
+
+// Pin Definitions
+#define HCSR04_PIN_TRIG	3
+#define HCSR04_PIN_ECHO	2
+#define LDR_PIN_SIG	A3
+#define RFID_PIN_SDA	10
+#define RFID_PIN_RST	4
+#define SERVO9G_PIN_SIG	5
+
+
+
+// Global variables and defines
+#define THRESHOLD_ldr   100
+int ldrAverageLight;
+const int servo9gRestPosition   = 20;  //Starting position
+const int servo9gTargetPosition = 150; //Position when event is detected
+// object initialization
+Servo servo9g;
+NewPing hcsr04(HCSR04_PIN_TRIG,HCSR04_PIN_ECHO);
+LDR ldr(LDR_PIN_SIG);
+RFID rfid(RFID_PIN_SDA,RFID_PIN_RST);
+RTC_DS3231 rtc;
+
+
+
+// define vars for testing menu
+const int timeout = 10000;       //define timeout of 10 sec
+char menuOption = 0;
+long time0;
+
+// Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
+void setup() 
+{
+    // Setup Serial which is useful for debugging
+    // Use the Serial Monitor to view printed messages
+    Serial.begin(9600);
+    while (!Serial) ; // wait for serial port to connect. Needed for native USB
+    Serial.println("start");
+    
+    ldrAverageLight = ldr.readAverage();
+    //initialize RFID module
+    rfid.init();
+    if (! rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        while (1);
+      }
+
+      if (rtc.lostPower()) {
+        Serial.println("RTC lost power, lets set the time!");
+        // following line sets the RTC to the date & time this sketch was compiled
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        // This line sets the RTC with an explicit date & time, for example to set
+        // January 21, 2014 at 3am you would call:
+        // rtcadjust(DateTime(2014, 1, 21, 3, 0, 0));
+      }
+    servo9g.attach(SERVO9G_PIN_SIG);
+    servo9g.write(servo9gRestPosition);
+    delay(100);
+    servo9g.detach();
+    menuOption = menu();
+    
+}
+
+// Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
+void loop() 
+{
+    
+    
+    if(menuOption == '1') {
+    // Ultrasonic Sensor - HC-SR04 - Test Code
+    // Read distance measurment from UltraSonic sensor           
+    int hcsr04Dist = hcsr04.ping_cm();
+    delay(10);
+    Serial.print(F("Distance: ")); Serial.print(hcsr04Dist); Serial.println(F("[cm]"));
+
+    }
+    else if(menuOption == '2') {
+    // LDR (Mini Photocell) - Test Code
+    // Get current light reading, substract the ambient value to detect light changes
+    int ldrSample = ldr.read();
+    int ldrDiff = abs(ldrAverageLight - ldrSample);
+    Serial.print(F("Light Diff: ")); Serial.println(ldrDiff);
+
+    }
+    else if(menuOption == '3') {
+    // RFID - RC522 RF IC Card Sensor Module - Test Code
+    //Read RFID tag if present
+    String rfidtag = rfid.readTag();
+    //print the tag to serial monitor if one was discovered
+    rfid.printTag(rfidtag);
+
+    }
+    else if(menuOption == '4') {
+    // DS3231 Precision RTC Breakout - Test Code
+    //This will display the time and date of the RTC. see RTC.h for more functions such as rtc.hour(), rtc.month() etc.
+    DateTime now = rtc.now();
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print('/');
+    Serial.print(now.year(), DEC);
+    Serial.print("  ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    delay(1000);
+    }
+    else if(menuOption == '5') {
+    // 9g Micro Servo - Test Code
+    // The servo will rotate to target position and back to resting position with an interval of 500 milliseconds (0.5 seconds) 
+    servo9g.attach(SERVO9G_PIN_SIG);         // 1. attach the servo to correct pin to control it.
+    servo9g.write(servo9gTargetPosition);  // 2. turns servo to target position. Modify target position by modifying the 'ServoTargetPosition' definition above.
+    delay(500);                              // 3. waits 500 milliseconds (0.5 sec). change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    servo9g.write(servo9gRestPosition);    // 4. turns servo back to rest position. Modify initial position by modifying the 'ServoRestPosition' definition above.
+    delay(500);                              // 5. waits 500 milliseconds (0.5 sec). change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    servo9g.detach();                    // 6. release the servo to conserve power. When detached the servo will NOT hold it's position under stress.
+    }
+    
+    
+    
+    if (millis() - time0 > timeout)
+    {
+        menuOption = menu();
+    }
+    
+}
+
+
+
+// Menu function for selecting the components to be tested
+// Follow serial monitor for instrcutions
+char menu()
+{
+
+    Serial.println(F("\nWhich component would you like to test?"));
+    Serial.println(F("(1) Ultrasonic Sensor - HC-SR04"));
+    Serial.println(F("(2) LDR (Mini Photocell)"));
+    Serial.println(F("(3) RFID - RC522 RF IC Card Sensor Module"));
+    Serial.println(F("(4) DS3231 Precision RTC Breakout"));
+    Serial.println(F("(5) 9g Micro Servo"));
+    Serial.println(F("(menu) send anything else or press on board reset button\n"));
+    while (!Serial.available());
+
+    // Read data from serial monitor if received
+    while (Serial.available()) 
+    {
+        char c = Serial.read();
+        if (isAlphaNumeric(c)) 
+        {
+            if(c == '1') 
+    			Serial.println(F("Now Testing Ultrasonic Sensor - HC-SR04"));
+    		else if(c == '2') 
+    			Serial.println(F("Now Testing LDR (Mini Photocell)"));
+    		else if(c == '3') 
+    			Serial.println(F("Now Testing RFID - RC522 RF IC Card Sensor Module"));
+    		else if(c == '4') 
+    			Serial.println(F("Now Testing DS3231 Precision RTC Breakout"));
+    		else if(c == '5') 
+    			Serial.println(F("Now Testing 9g Micro Servo"));
+            else
+            {
+                Serial.println(F("illegal input!"));
+                return 0;
+            }
+            time0 = millis();
+            return c;
+            }
+        }
+    }
+
+/*******************************************************
+
+*    Circuito.io is an automatic generator of schematics and code for off
+*    the shelf hardware combinations.
+
+*    Copyright (C) 2016 Roboplan Technologies Ltd.
+
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*    In addition, and without limitation, to the disclaimers of warranties 
+*    stated above and in the GNU General Public License version 3 (or any 
+*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
+*    program subject to the following warranty disclaimers and by using 
+*    this program you acknowledge and agree to the following:
+*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
+*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
+*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
+*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
+*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
+*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
+*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
+*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
+*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
+*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
+*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
+*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
+*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
+*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
+*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
+*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
+*    THE ABOVE. 
+********************************************************/
